b3eb34856a63fe9cd02d8898d21b5aa23a92b724
Stephen Weiss <stephencweiss@gmail.com>
2018-10-14 11:56:36 -0700

    removed commented lines

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express'),
   http = require('http'),
   port = 80,
   express = require('express'),
-    socketIo = require('socket.io'),
+  socketIo = require('socket.io'),
   bodyParser = require('body-parser');
   
   
@@ -17,21 +17,21 @@ app.use(bodyParser);
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.all('/*', function (req, res, next) {
-    //CORS headers
-    res.header({
-        'access-control-allow-origin':'*',
-        'access-control-allow-methods':'GET, POST, PUT, DELETE, OPTIONS',
-        'access-control-allow-headers':'Content-Type, Accept, X-Access-Token, X-Key'
-    })
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-    } else {
-        next();
-    }
+  //CORS headers
+  res.header({
+    'access-control-allow-origin':'*',
+    'access-control-allow-methods':'GET, POST, PUT, DELETE, OPTIONS',
+    'access-control-allow-headers':'Content-Type, Accept, X-Access-Token, X-Key'
+  })
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
 });
 
 app.get('/', function (req, res){
-    res.send('Real time POS web app running.');
+  res.send('Real time POS web app running.');
 })
 
 app.use('/api/inventory', require('./api/inventory'));
@@ -40,26 +40,26 @@ app.use('/api', require('.api/transactions'));
 // Websocket logic for live cart
 
 io.on ('connection', function (socket) {
-    socket.on('cart-transaction-complete', function() {
-        socket.broadcast.emit('update-live-cart-dispplay', {});
-    });
-    
-    //on page load, show user current cart
-    socket.on('live-cart-page-loaded', function() {
-        socket.emit('update-live-cart-display', liveCart);
-    });
-    
-    // when client connected, make client update live cart
+  socket.on('cart-transaction-complete', function() {
+    socket.broadcast.emit('update-live-cart-dispplay', {});
+  });
+
+  //on page load, show user current cart
+  socket.on('live-cart-page-loaded', function() {
     socket.emit('update-live-cart-display', liveCart);
+  });
 
-    // when the cart data is updated by the POST
-    socket.on('update-live-cart', function(cartData) {
-        // keep track of the change
-        liveCart = cartData;
+  // when client connected, make client update live cart
+  socket.emit('update-live-cart-display', liveCart);
 
-        // broadcast updated live cart to all websocket clients
-        socket.broadcasat.emit('update-live-cart-display', liveCart);
-    });
+  // when the cart data is updated by the POST
+  socket.on('update-live-cart', function(cartData) {
+    // keep track of the change
+    liveCart = cartData;
+
+    // broadcast updated live cart to all websocket clients
+    socket.broadcasat.emit('update-live-cart-display', liveCart);
+   });
 });
 
 server.listen(port, () => console.log(`listening on port ${port}`));
