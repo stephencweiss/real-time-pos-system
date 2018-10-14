fd27351fa8bfea6ad91a73e1378740f70b263608
Stephen Weiss <stephencweiss@gmail.com>
2018-10-14 14:34:58 -0700

    moved api files

---
+++ api/inventory.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var Datastore = require('nedb');
+
+var app = express();
+var server = http.Server(app);
+
+app.use(bodyParser.json());
+
+module.exports = app;
+
+
+// Create Database
+var inventoryDB = new Datastore( {
+  filename: './server/databases/inventory.db',
+  autoload: true
+});
+
+// GET inventory
+app.get('/', function (req, res) {
+  res.send('Inventory API')
+})
+
+// GET a product from inventory by _id
+app.get('/product/:productID', function(req, res) {
+  if (!req.params.productId) {
+    res.status(500).send('ID field required.');
+  } else {
+    //nedB.findOne(query, callback)
+    inventoryDB.findOne({ _id: req.params.productId}, function (err, product){
+      res.send(product)
+    });
+  }
+})
+
+// GET all inventory products
+app.get('/products', function (req, res) {
+  //nedB.find(query,callback)
+  inventoryDB.find({}, function (err, products) {
+    console.log(`sending all products`);
+    res.send(products);
+  })
+})
+
+// Create an inventory product
+app.post('/products', function(req, res) {
+  var newProduct = req.body;
+  //nedB.insert(doc, callback)
+  inventoryDB.insert(newProduct , function (err, product){
+    if(err) res.status(500).send(err);
+    else res.send(product)
+  });
+})
+
+// Delete an inventory product?
+app.delete('/product/:productId', function (req, res){
+  //nedB.remove(query, callback)
+  inventoryDB.remove({ _id: req.params.productId }, function (err, numRemoved){
+    if (err) res.status(500).send(err);
+    else res.status(200).send();
+  });
+})
+
+// Updates Inventory product
+app.put('/product', function (req, res) {
+  var productId = req.body._id;
+  //nedB.update(query, update, options, callback)
+  inventoryDB.update({ _id: productId}, req.body, {}, function (err, numReplaced, product){
+    if (err) res.status(500).send(err);
+    else res.status(200).send();
+  });
+})
+
+app.decrementInventory = function(products) {
+  async.eachSeries(products, function(transactionProduct, callback){
+    //nedB.findOne(query, callback)
+    inventoryDB.findOne({ _id: transactionProduct._id }, function (err, product) {
+      // Catch manually added items (which don't exist in inventory)
+      if (!product || !product.quantity_on_hand) {
+        callback();
+      } else {
+        var updatedQuantity = parseInt(product.quantity_on_hand) - parseInt(transactionProduct.quantity);
+        //nedB.update(query, update, options, callback)
+        inventoryDB.update({_id: product._id}, { $set: { quantity_on_hand: updatedQuantity } }, {}, callback);
+      }
+    });
+  });
+}
---
+++ api/transactions.js
@@ -0,0 +1,119 @@
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var Datastore = require('nedb');
+
+var app = express();
+var server = http.Server(app);
+
+var Inventory = require('./inventory');
+
+module.exports = app;
+
+// Helper functions
+function setDate(date){
+  var returnDate = {};
+  returnDate.startDate = date ? new Date(date) : new Date();
+  returnDate.endDate = date ? new Date(date) : new Date();
+  returnDate.startDate.setHours(0,0,0,0)
+  returnDate.endDate.setHours(23,59,59,59)
+  return returnDate;
+}
+
+// Create database
+var Transactions = new Datastore({
+  filename: './server/databases/transactions.db',
+  autoload: true
+});
+
+app.get('/', function(req, res) {
+  res.send('Transactions API')
+});
+
+// GET all transactions
+app.get('/all', function(req, res) {
+  //nedb.find(query, callback)
+  Transactions.find({}, function (err, transactions) {
+    if (err) res.status(500).send(err);
+    else res.status(201).send(transactions) 
+  });
+});
+
+// GET limited transactions
+app.get('/limit', function (req, res) {
+  var limit = parseInt(req.query.limit, 10) || 5
+  Transactions.find({}, function (err) {
+    if (err) res.status(500).send();
+  })
+    .sort({ date : -1 })
+    .exec(function (err, transactions) {
+      if (err) res.status(500).send();
+      else res.status(201).send(transactions) 
+    })
+});
+
+// GET total sales for current day
+app.get('/day-total', function (req, res) {
+  // if day is provided
+  var searchDate = setDate(req.query.date);
+  //nedb.find(query, callback)
+  Transactions.find(
+    {date: { 
+      $gte : searchDate.startDate.toJSON(), 
+      $lte : searchDate.endDate.toJSON()
+    }}, function (err, transactions) {
+      if (err) res.status(500).send(err);
+      else{
+        var result = { date : searchDate.startDate};
+        if (transactions) {
+          var total = transactions.reduce((total, current) => {return total + current}, 0.00);
+          result.total = parseFloat(parseFloat(total).toFixed(2));
+        } else {
+          result.total = 0;
+        }
+        res.status(200).send(result);
+      }
+    }
+  )
+})
+
+// GET transactions for a particular date
+app.get('/by-date', function (req, res) {
+  var searchDate = setDate(req.query.date)
+  Transactions.find( 
+    { date: { 
+    $gte : searchDate.startDate.toJSON(), 
+    $lte : searchDate.endDate.toJSON()
+    }}, function (err, transactions) {
+      if (err) res.status(500).send(err);
+      else {
+        if (transactions) {
+          res.status(200).send(transactions);
+        }
+      }
+    }
+  );
+})
+
+// Add a new transaction
+app.post('/new', function (req, res) {
+  var newTransaction = req.body;
+  //nedB.insert(doc, callback)
+  Transactions.insert(newTransaction, function (err, transaction) {
+    if (err) res.status(500).send(err);
+    else {
+      res.status(200).sendDate();
+      Inventory.decrementInventory(transaction.products);
+    }
+  });
+})
+
+// GET a single transaction
+app.get('/:transactionID', function (req, res) {
+  Transactions.find({ _id: req.params.transactionID }, function(err, transaction) {
+    if (err) res.status(500).send(err);
+    else if (transaction) {
+      res.status(200).send(transaction[0]);
+    }
+  })
+})
