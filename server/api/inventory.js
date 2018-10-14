6da09210d6860f35ed18fe2d8f5731461b8d1116
Stephen Weiss <stephencweiss@gmail.com>
2018-10-14 12:32:17 -0700

    added inventory management route

--- server/api/inventory.js
+++ server/api/inventory.js
@@ -66,5 +66,24 @@ app.delete('/product/:productId', function (req, res){
 app.put('/product', function (req, res) {
   var productId = req.body._id;
   //nedB.update(query, update, options, callback)
-  inventoryDB.update({ _id: productId}, req.body, {}, function ())
+  inventoryDB.update({ _id: productId}, req.body, {}, function (err, numReplaced, product){
+    if (err) res.status(500).send(err);
+    else res.status(200).send();
+  });
 })
+
+app.decrementInventory = function(products) {
+  async.eachSeries(products, function(transactionProduct, callback){
+    //nedB.findOne(query, callback)
+    inventoryDB.findOne({ _id: transactionProduct._id }, function (err, product) {
+      // Catch manually added items (which don't exist in inventory)
+      if (!product || !product.quantity_on_hand) {
+        callback();
+      } else {
+        var updatedQuantity = parseInt(product.quantity_on_hand) - parseInt(transactionProduct.quantity);
+        //nedB.update(query, update, options, callback)
+        inventoryDB.update({_id: product._id}, { $set: { quantity_on_hand: updatedQuantity } }, {}, callback);
+      }
+    });
+  });
+}
